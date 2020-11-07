3b015de3d8dba5be477f1f12d5b6cb3ad77ef00d
adhiesg <adhienugroho53@gmail.com>
2020-11-07 18:12:27 +0700

    del pdfworker

--- public/app.js
+++ public/app.js
@@ -1,15 +1,15 @@
 // If absolute URL from the remote server is provided, configure the CORS
 // header on that server.
 var url =
-  "https://raw.githubusercontent.com/mozilla/pdf.js/ba2edeae/web/compressed.tracemonkey-pldi-09.pdf";
-  //"https://github.com/adhiesg/Maybank-CCI/blob/master/public/test.pdf?raw=true";
+  //"https://raw.githubusercontent.com/mozilla/pdf.js/ba2edeae/web/compressed.tracemonkey-pldi-09.pdf";
+  "https://github.com/adhiesg/Maybank-CCI/blob/master/public/test.pdf?raw=true";
 
 // Loaded via <script> tag, create shortcut to access PDF.js exports.
 var pdfjsLib = window["pdfjs-dist/build/pdf"];
 
 // The workerSrc property shall be specified.
 pdfjsLib.GlobalWorkerOptions.workerSrc =
-  "//mozilla.github.io/pdf.js/build/pdf.worker.js";
+  "https://github.com/adhiesg/Maybank-CCI/blob/master/public/pdfworker.js";
 
 var pdfDoc = null,
   pageNum = 1,
