de8c7bea0fceeab7098acad25ea255f8eb4cbfff
adhiesg <adhienugroho53@gmail.com>
2020-11-11 23:03:50 +0700

    samain sama kaya versi asli
    tes

---
+++ public/full.js
@@ -0,0 +1,78 @@
+const url = 'https://raw.githubusercontent.com/mozilla/pdf.js/ba2edeae/web/compressed.tracemonkey-pldi-09.pdf';
+
+//const url = './test.pdf';
+
+let pdfDoc = null,
+  pageNum = 1,
+  pageIsRendering = false,
+  pageNumIsPending = null,
+  scale = 1;
+
+const canvasContainer = document.getElementById('holder');
+
+const renderPage = (num) => {
+  pageIsRendering = true;
+  pdfDoc.getPage(num).then((page) => {
+    const viewport = page.getViewport({ scale: scale });
+    const wrapper = document.createElement('div');
+    wrapper.className = 'canvas-wrapper';
+    const canvas = document.createElement('canvas');
+    const context = canvas.getContext('2d');
+    canvas.height = viewport.height;
+    canvas.width = viewport.width;
+
+    const renderContext = {
+      canvasContext: context,
+      viewport,
+    };
+
+    wrapper.appendChild(canvas);
+    canvasContainer.appendChild(wrapper);
+    page.render(renderContext);
+  });
+};
+
+const renderPages = () => {
+  for (let num = 1; num <= pdfDoc.numPages; num++) {
+    pdfDoc.getPage(num).then(renderPage(num));
+  }
+};
+
+pdfjsLib.getDocument(url).promise.then((pdfDoc_) => {
+  pdfDoc = pdfDoc_;
+  // document.querySelector('#page-count').textContent = pdfDoc.numPages;
+  console.log(pdfDoc);
+
+  // renderPages(pageNum);
+  // renderPage(pageNum);
+  // for (let num = 1; num <= pdfDoc.numPages; num++) {
+  //   pdfDoc.getPage(num).then(renderPage(num));
+  // }
+  renderPages();
+});
+
+// Zoom In
+const zoomIn = () => {
+  if (scale >= 5) {
+    return;
+  } else {
+    scale = scale + 0.25;
+    // queueRenderPage(pageNum);
+
+    document.querySelectorAll('.canvas-wrapper').forEach((e) => e.remove());
+
+    renderPages();
+  }
+};
+document.querySelector('#zoom-in').addEventListener('click', zoomIn);
+
+// Zoom Out
+const zoomOut = () => {
+  if (scale <= 1) {
+    return;
+  } else {
+    scale = scale - 0.25;
+    queueRenderPage(pageNum);
+  }
+};
+document.querySelector('#zoom-out').addEventListener('click', zoomOut);
---
+++ public/single.js
@@ -0,0 +1,106 @@
+// PDF Location
+const url = 'https://raw.githubusercontent.com/mozilla/pdf.js/ba2edeae/web/compressed.tracemonkey-pldi-09.pdf';
+
+// The PDF Document itself
+let pdfDoc = null,
+  pageNum = 1,
+  pageIsRendering = false,
+  pageNumIsPending = null,
+  scale = 1;
+
+const canvas = document.querySelector('#pdf-render'),
+  ctx = canvas.getContext('2d');
+
+// Render Page Function
+const renderPage = (num) => {
+  pageIsRendering = true;
+
+  // Get Page
+  pdfDoc.getPage(num).then((page) => {
+    // console.log(page);
+    const viewport = page.getViewport({ scale: scale });
+    canvas.height = viewport.height;
+    canvas.width = viewport.width;
+
+    const renderCtx = {
+      canvasContext: ctx,
+      viewport,
+    };
+
+    page.render(renderCtx).promise.then(() => {
+      pageIsRendering = false;
+
+      if (pageNumIsPending != null) {
+        renderPage(pageNumIsPending);
+        pageNumIsPending = null;
+      }
+    });
+
+    // Print current page number
+    document.querySelector('#page-num').textContent = num;
+  });
+};
+
+// Check for pages rendering
+const queueRenderPage = (num) => {
+  if (pageIsRendering) {
+    pageNumIsPending = num;
+  } else {
+    renderPage(num);
+  }
+};
+
+// Next Page
+
+const nextPage = () => {
+  if (pageNum >= pdfDoc.numPages) {
+    return;
+  } else {
+    pageNum++;
+    queueRenderPage(pageNum);
+  }
+};
+document.querySelector('#next').addEventListener('click', nextPage);
+
+// Prev Page
+
+const prevPage = () => {
+  if (pageNum <= 1) {
+    return;
+  } else {
+    pageNum--;
+    queueRenderPage(pageNum);
+  }
+};
+document.querySelector('#prev').addEventListener('click', prevPage);
+
+// Get Document
+pdfjsLib.getDocument(url).promise.then((pdfDoc_) => {
+  pdfDoc = pdfDoc_;
+  document.querySelector('#page-count').textContent = pdfDoc.numPages;
+  console.log(pdfDoc);
+
+  renderPage(pageNum);
+});
+
+// Zoom In
+const zoomIn = () => {
+  if (scale >= 5) {
+    return;
+  } else {
+    scale = scale + 0.25;
+    queueRenderPage(pageNum);
+  }
+};
+document.querySelector('#zoom-in').addEventListener('click', zoomIn);
+
+// Zoom Out
+const zoomOut = () => {
+  if (scale <= 1) {
+    return;
+  } else {
+    scale = scale - 0.25;
+    queueRenderPage(pageNum);
+  }
+};
+document.querySelector('#zoom-out').addEventListener('click', zoomOut);
